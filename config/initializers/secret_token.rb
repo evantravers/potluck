15b764496768d0efa730699dda2807fcb6bc69e1
Evan Travers <evantravers@gmail.com>
2012-01-10 17:42:46 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Potluck::Application.config.secret_token = '0cd621cd4fd54ac0e2c0f1867eea73b2ce16f0ec59c5ff47ca34e6df46cfa29b570d437e90f0292707aa1eaa69555d4cacecff7d46b107ce23984777ff6b9ab9'
